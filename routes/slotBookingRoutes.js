9b452ff9a726d34194a8b4688b98abe15e3529d0
Vandlamudi Pradeep <118242335+VadlamudiPradeep@users.noreply.github.com>
2023-07-13 16:23:48 +0530

    slotBookingRoutes.js

--- routes/slotBookingRoutes.js
+++ routes/slotBookingRoutes.js
@@ -5,6 +5,6 @@ let router = express.Router();
 let slotBookingControllers = require('../controllers/slotBookingControllers');
 
 router.post('/slot' , slotBookingControllers.sloBook);
-
+router.get('/slotGet', slotBookingControllers.slotGet)
 
 module.exports = router ; 
